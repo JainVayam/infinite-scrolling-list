e96f4de922e85d2b2343547693024c6f3479a415
Vayam Jain <vayam.jain@gmail.com>
2021-04-30 02:06:50 +0530

    Added changes to the UI

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,17 +4,29 @@ import {
 } from '@material-ui/core'
 
 import useStyles from './style'
+import useLoginController from './controller'
 
 const Login = () => {
   const classes = useStyles()
+  const { onSubmit, onChange } = useLoginController()
   return (
     <Container className={ classes.loginContainer }>
       <Card className={ classes.loginCard }>
         <h1> LOGIN </h1>
         <form className={ classes.form }>
-          <TextField fullWidth className={ classes.formFields } placeholder='Username' />
-          <TextField fullWidth className={ classes.formFields } placeholder='Password' />
-          <Button> Log In</Button>
+          <TextField
+            onChange={ (e) => onChange('username', e) }
+            fullWidth
+            className={ classes.formFields }
+            placeholder='Username'
+          />
+          <TextField
+            onChange={ (e) => onChange('password', e) }
+            fullWidth
+            className={ classes.formFields }
+            placeholder='Password'
+          />
+          <Button onClick={ onSubmit }> Log In</Button>
         </form>
       </Card>
     </Container>
