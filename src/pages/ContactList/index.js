e4fe4c75cb1f7e5d9f53ee189115c849f15ac068
Vayam Jain <vayam.jain@gmail.com>
2021-04-30 03:55:56 +0530

    Added onscroll

--- src/pages/ContactList/index.js
+++ src/pages/ContactList/index.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import {
-  Container, List, ListItem, ListItemIcon, ListItemText, Paper, Divider,
+  Container, List, ListItem, ListItemIcon, ListItemText, Divider,
 } from '@material-ui/core'
 
-import { contacts } from '../../utils/constants/dummyData'
+import useContactListController from './controller'
 import useStyles from './style'
 
 const ContactList = () => {
   const classes = useStyles()
+  const { onScroll, contactList, ref } = useContactListController()
+
   return (
-    <Container className={ classes.container }>
+    <Container ref={ ref } onScroll={ onScroll } className={ classes.container }>
       <List className={ classes.list }>
-        {contacts.map(({ name, image }) => (
-          <>
+        {contactList.map(({ name, image, id }) => (
+          <div key={ id }>
             <ListItem>
               <ListItemIcon>
                 <img className={ classes.contactImage } alt='N/A' src={ image } />
@@ -20,7 +22,7 @@ const ContactList = () => {
               <ListItemText className={ classes.listText } primary={ name } />
             </ListItem>
             <Divider />
-          </>
+          </div>
         ))}
       </List>
     </Container>
