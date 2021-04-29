1d26bba6265100bf8ce9e5bbad0f49becb562c9d
JainV <35289918+VJHunt@users.noreply.github.com>
2021-04-30 04:02:22 +0530

    Feature/infinite list (#3)
    * Added contacts page
    
    * Added list route
    
    * Added contact dummy data
    
    * Improved UI
    
    * Added controller for contact list
    
    * Corrected ummy contact lists data
    
    * Added onscroll

--- src/routes/routesList.js
+++ src/routes/routesList.js
@@ -8,7 +8,11 @@ const routesList = [ {
 }, {
   path: '/home',
   component: ContactList,
+<<<<<<< 0caa6df5c8d654ee2891ccb1070f994980de3795
   isAuth: true,
+=======
+  isAuth: false,
+>>>>>>> Feature/infinite list (#3)
 } ]
 
 export default routesList
