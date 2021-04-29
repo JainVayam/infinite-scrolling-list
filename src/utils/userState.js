0caa6df5c8d654ee2891ccb1070f994980de3795
JainV <35289918+VJHunt@users.noreply.github.com>
2021-04-30 02:11:25 +0530

    Feature/login (#1)
    * Added react router
    
    * Added dummy data file
    
    * Added routes folder for routing
    
    * Added custom router
    
    * Added routes list file
    
    * Added route validator
    
    * Added basic user state file for local storage management
    
    * Added routing parent
    
    * Added login UI
    
    * Changed credentials data shape
    
    * Added controller methods
    
    * Added changes to the UI
    
    * Included routes in the app
    
    * Added prop types package
    
    * Defined proptypes
    
    * Changed linter settings

--- src/utils/userState.js
+++ src/utils/userState.js
@@ -1,5 +1,9 @@
 const setKey = (key, data) => localStorage.setItem(key, data)
 
+<<<<<<< 6c9aab4ba609156ab040ad7d50364d26d1fe42b5
 const isLoggedIn = () => localStorage.getItem('isLoggedIn')
 
 export { setKey, isLoggedIn }
+=======
+const isUserLoggedIn = () => localStorage.getItem('isLoggedIn')
+>>>>>>> Feature/login (#1)
