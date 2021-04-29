5fd7ce5f34cfa7e3fa7f9a13cdb220ad4f5ff2f1
Vayam Jain <vayam.jain@gmail.com>
2021-04-30 02:30:33 +0530

    Added list route

--- src/routes/routesList.js
+++ src/routes/routesList.js
@@ -1,8 +1,13 @@
 import Login from '../pages/Login'
+import ContactList from '../pages/ContactList'
 
 const routesList = [ {
   path: '/login',
   component: Login,
+  isAuth: false,
+}, {
+  path: '/home',
+  component: ContactList,
   isAuth: false,
 } ]
 
