3c7561816b61ba93a76bccbe80ea664a3fe8580c
JainV <35289918+VJHunt@users.noreply.github.com>
2021-04-30 04:34:04 +0530

    Fix/auth check (#5)
    * Added auth check for homepage
    
    * Added redirection
    
    * Added error display on wrong credentials

--- src/routes/routeValidator.js
+++ src/routes/routeValidator.js
@@ -2,20 +2,12 @@ import React from 'react'
 import { Redirect } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-<<<<<<< 6c9aab4ba609156ab040ad7d50364d26d1fe42b5
 import * as userState from '../utils/userState'
-=======
-import userState from '../utils/userState'
->>>>>>> Feature/login (#1)
 
 const RouteValidator = ({
   isAuth, component: Component, path,
 }) => {
-<<<<<<< 6c9aab4ba609156ab040ad7d50364d26d1fe42b5
   if (path === '/login' && userState.isLoggedIn()) return <Redirect to='/home' />
-=======
-  if (path === '/') return <Redirect to='/home' />
->>>>>>> Feature/login (#1)
   if (isAuth) {
     if (userState.isLoggedIn()) {
       return (
